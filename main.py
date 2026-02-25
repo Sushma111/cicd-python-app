0758cf796be9a1b08687dd65614ded384938a106
Sushma <sushmalagisetty2022@gmail.com>
2026-02-25 15:47:18 +0530

    Initial commit -FastAPI app

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FASTAPI
+app = FASTAPI()
+users = []
+@app.get("/")
+def home():
+    return{"message":"User service running"}
+@app.post("/users")
+def create_user(name :str):
+    users.append(name)
+    return{"message":f"user {name} added"}
+@app.get("/users"):
+    return {"users": users}
+
+
+
+
